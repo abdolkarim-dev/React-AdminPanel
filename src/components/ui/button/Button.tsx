fe5f30d873a77c53b4177ef7c488fce4f317c8be
abdolkarim-dev <bazdarabdolkarim@gmail.com>
2026-09-01 01:15:23 +0330

    Refactor: update and optimize button component

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -6,8 +6,6 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "outline";
   startIcon?: ReactNode;
   endIcon?: ReactNode;
-  disabled?: boolean;
-  className?: string;
 }
 
 const Button = ({
@@ -16,8 +14,7 @@ const Button = ({
   variant = "primary",
   startIcon,
   endIcon,
-  className = "",
-  disabled = false,
+  className = "", 
   type = "button",
   ...props
 }: ButtonProps) => {
@@ -30,7 +27,7 @@ const Button = ({
     primary:
       "bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300",
     outline:
-      "bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white/[0.03] dark:hover:text-gray-300",
+      "bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50  ",
   };
 
   return (
