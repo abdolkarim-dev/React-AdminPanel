237cde5b7981ab2e66da4e57f3d50b78fc9e8905
abdolkarim-dev <bazdarabdolkarim@gmail.com>
2026-09-03 10:35:05 +0330

    feat: add a Auth loyout

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router";
-import { ChevronLeftIcon, EyeCloseIcon, EyeIcon } from "../../icons";
-import Label from "../form/Label";
+import { ChevronLeftIcon, EyeCloseIcon, EyeIcon } from "@/assets/icons";
+ import Label from "../form/Label";
 import Input from "../form/input/InputField";
 import Checkbox from "../form/input/Checkbox";
 import Button from "../ui/button/Button";
