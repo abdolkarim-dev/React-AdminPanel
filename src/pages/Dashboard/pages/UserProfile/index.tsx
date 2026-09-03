c5adab76513d87132a965144b02aa83bfcd22bd0
abdolkarim-dev <bazdarabdolkarim@gmail.com>
2026-09-03 08:35:50 +0330

    fix: change style padding for profile page

--- src/pages/Dashboard/pages/UserProfile/index.tsx
+++ src/pages/Dashboard/pages/UserProfile/index.tsx
@@ -1,10 +1,13 @@
-import type { JSX } from "react"; 
+import type { JSX } from "react";
 import UserMetaCard from "@/components/UserProfile/UserMetaCard";
 import UserInfoCard from "@/components/UserProfile/UserInfoCard";
 import UserAddressCard from "@/components/UserProfile/UserAddressCard";
-function UserProfile():JSX.Element {
-    return (
-       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
+import { div } from "framer-motion/client";
+function UserProfile(): JSX.Element {
+  return (
+    <div className="p-8">
+      
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
         <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
           Profile
         </h3>
@@ -14,7 +17,8 @@ function UserProfile():JSX.Element {
           <UserAddressCard />
         </div>
       </div>
-    )
+    </div>
+  );
 }
 
 export default UserProfile;
